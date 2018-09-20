69f025fa183f607d676b73df2fc66f3983e71915
shikhadaka179 <43431042+shikhadaka179@users.noreply.github.com>
2018-09-20 06:22:25 -0400

    Update s.java

--- s.java
+++ s.java
@@ -1,6 +1,11 @@
 class public Shikha{
 
 	public static void main(String args[]) {
-		System.out.println("Hello World");	
+		System.out.println("Hello World");
+		for(i=0;i<10;i++)
+		{
+			System.out.println(i*i);
+		}
+		
 	}
 }
